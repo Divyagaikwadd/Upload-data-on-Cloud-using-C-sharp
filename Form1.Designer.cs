7395aa3a033425246c34056d55bd75e1fa269342
Divya Gaikwad <99081628+Divyagaikwadd@users.noreply.github.com>
2023-02-21 19:45:13 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace TestAccess1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnSave.Location = new System.Drawing.Point(120, 268);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(113, 33);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 114);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter your Name :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(261, 114);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(393, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.Location = new System.Drawing.Point(305, 268);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 33);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Upload on Cloud";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(839, 460);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSave);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.OleDb;  //OLE DB (Object Linking and Embedding, Database) technology. OLE DB is a Microsoft data access technology
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestAccess1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {   
+
+            //  insert data inside the microsoft access
+
+            OleDbConnection conn = null;
+            conn = new OleDbConnection();
+            conn.ConnectionString = ConfigurationManager.ConnectionStrings["Connection"].ToString();
+            conn.Open();
+            OleDbCommand cmd = new OleDbCommand();
+            cmd.CommandText = "insert into[Employee](Name1)values(@nm)";
+            cmd.Parameters.AddWithValue("@nm", textBox1.Text);
+            cmd.Connection = conn;
+            int a = cmd.ExecuteNonQuery();
+            if (a > 0)
+            {
+                MessageBox.Show("inserted");
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //code to fech the data from access database to in the list
+
+                                      //pass the path of microsoft access database
+            string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\ADMIN\\Documents\\Database3.accdb";
+            OleDbConnection connection = new OleDbConnection(connectionString);
+
+            // Open the connection
+            connection.Open();
+
+            // Create a command to retrieve the column names
+            OleDbCommand command = new OleDbCommand("SELECT * FROM  Employee", connection);
+
+            // Execute the command and retrieve the column names
+            OleDbDataReader reader = command.ExecuteReader();
+            List<string> columnNames = new List<string>();
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                columnNames.Add(reader.GetName(i));
+            }
+
+            // Create a list to store the data
+            List<List<object>> data = new List<List<object>>();
+
+            // Retrieve the data for each row
+            while (reader.Read())
+            {
+                List<object> row = new List<object>();
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    row.Add(reader.GetValue(i));
+                }
+                data.Add(row);
+            }
+          
+
+
+
+            //  code to connect with mongodb atlas database  uploading the data through list
+                                          
+            var client = new MongoClient("pass the link of mongodb atlas application with your username & password");
+
+            var database = client.GetDatabase("AccessDB"); //database name on mongodb atlas
+
+            // Create a new collection                           //column name
+            var collection = database.GetCollection<BsonDocument>("Employee");
+
+            // Create a list of BsonDocuments to insert
+            List<BsonDocument> documents = new List<BsonDocument>();
+            foreach (var row in data)
+            {
+                var document = new BsonDocument();
+                for (int i = 0; i < columnNames.Count; i++)
+                {
+                    document.Add(columnNames[i], BsonValue.Create(row[i]));
+                }
+                documents.Add(document);
+            }
+
+            // Insert the documents into the collection
+            collection.InsertMany(documents);
+            MessageBox.Show("Successful on cloud");
+           
+
+
+
+
+
+        }
+    }
+}
